6a07ead7ccdf28c6a46c42fd43eb8a8e5a3c7a01
karwackid <damian.karwacki@slaskie.pl>
2021-06-15 08:26:13 +0200

    refactoring

--- resources/views/layouts/odrers/edit.blade.php
+++ resources/views/layouts/odrers/edit.blade.php
@@ -6,7 +6,6 @@
     </div>
     <div class="row justify-content-center d-grid">
         <div class="col overflow-scroll" style="height: 500px">
-            {{session('edit')}}
         @include('layouts/products/plates')
         @include('layouts/products/products')
         </div>
