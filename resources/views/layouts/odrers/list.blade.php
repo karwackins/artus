8be0908cb920079423126bbb0bc059633e0b7e56
karwackid <damian.karwacki@slaskie.pl>
2021-06-16 22:24:17 +0200

    formatowanie listy zamowien

--- resources/views/layouts/odrers/list.blade.php
+++ resources/views/layouts/odrers/list.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 <div class="container-fluid">
-    <table class="table table-striped">
+    <div class="table-responsive-md"></div>
+    <table class="table table-striped table-bordered" style="font-size: small">
         <thead>
             <th width="5%">L.p</th>
-            <th width="10%">Data</th>
-            <th width="10%">Nazwisko zamawiającego</th>
-            <th width="10%">Telefon kontaktowy</th>
-            <th width="10%">Data realizacji</th>
+            <th width="15%">Data</th>
+            <th width="10%">Klient</th>
+            <th width="10%">Telefon</th>
+            <th width="15%">Data realizacji</th>
             <th width="20%">Uwagi</th>
             <th width="5%">Kwota</th>
-            <th width="30%" colspan="2">Operacje</th>
+            <th width="20%" colspan="2">Operacje</th>
         </thead>
         <tbody>
         @foreach($orders as $order)
@@ -40,18 +41,24 @@
                 <td>
                     <div class="d-block">
                         @if($order['status'] == 0)
-                            <a href="/pdf-kitchen/{{$order['id']}}" class="btn-lg btn-primary"><i class="fas fa-fire"></i></a>
-                            <a href="/pdf-customer/{{$order['id']}}" class="btn-lg btn-primary"><i class="fas fa-print"></i></a>
+                            <a href="/pdf-kitchen/{{$order['id']}}" class="btn-lg btn-primary m-1"><i class="fas fa-fire"></i></a>
+                            <a href="/pdf-customer/{{$order['id']}}" class="btn-lg btn-primary m-1"><i class="fas fa-print"></i></a>
                         @else
                         <form action="/order/{{$order['id']}}" method="POST">
-                            <a href="/pdf-kitchen/{{$order['id']}}" class="btn-lg btn-primary"><i class="fas fa-fire"></i></a>
-                            <a href="/pdf-customer/{{$order['id']}}" class="btn-lg btn-primary"><i class="fas fa-print"></i></a>
-                            <a href="/edit-customer/{{$order['id']}}" class="btn-lg btn-success"><i class="fas fa-user-edit"></i></a>
-                            <a href="/order/{{$order['id']}}/edit" class="btn-lg btn-success"><i class="fas fa-edit"></i></a>
-                            <input type="hidden" name="_method" value="DELETE">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <button type="submit" class="btn-lg btn-group-sm btn-danger delete-user"><i class="fas fa-trash-alt"></i></button>
-                            <a href="/update-status/{{$order['id']}}" class="btn-lg btn-group-sm btn-primary"><i class="fas fa-check"></i></a>
+                            <div class="container">
+                                <div class="row">
+                                    <a href="/pdf-kitchen/{{$order['id']}}" class="btn-lg btn-primary m-0"><i class="fas fa-fire"></i></a>
+                                    <a href="/pdf-customer/{{$order['id']}}" class="btn-lg btn-primary m-0"><i class="fas fa-print"></i></a>
+                                    <a href="/update-status/{{$order['id']}}" class="btn-lg btn-primary m-0"><i class="fas fa-check"></i></a>
+                                </div>
+                                <div class="row">
+                                    <a href="/edit-customer/{{$order['id']}}" class="btn-lg btn-success m-0"><i class="fas fa-user-edit"></i></a>
+                                    <a href="/order/{{$order['id']}}/edit" class="btn-lg btn-success m-0"><i class="fas fa-edit"></i></a>
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <button type="submit" class="btn-lg btn-group-sm btn-danger delete-user m-0"><i class="fas fa-trash-alt"></i></button>
+                                </div>
+                            </div>
                         </form>
                         @endif
                     </div>
@@ -60,6 +67,7 @@
         @endforeach
         </tbody>
     </table>
+</div>
 </div>
 @endsection
 
