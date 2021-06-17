3b9a1185388686059dd901c06d7f825e6157395b
karwackid <damian.karwacki@slaskie.pl>
2021-06-17 11:20:51 +0200

    formatowanie total

--- resources/views/layouts/cart/cart_save.blade.php
+++ resources/views/layouts/cart/cart_save.blade.php
@@ -3,7 +3,7 @@
             @csrf <!-- {{ csrf_field() }} -->
                 <input type="hidden" name="_method" value="PUT">
                 <label for="suma">Suma:</label>
-                <input type="text" class="form-control" name="total" value="{{ $total }}">
+                <input type="text" class="form-control" name="total" value="{{ number_format($total, 2) }}">
                 <br />
                 <button type="submit" class="btn btn-lg btn-success">Aktualizuj zamówienie</button>
      </form>
@@ -15,7 +15,7 @@
     <form action="/save-order" method="POST">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <label for="suma">Suma</label>
-        <input type="text" name="total" class="form-control" value="{{ $total }}">
+        <input type="text" name="total" class="form-control" value="{{ number_format($total, 2) }}">
         <hr>
         <button type="submit" class="btn btn-lg btn-success">Zapisz zamówienie</button>
     </form>
